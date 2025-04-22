bd83561dbe5aff2e258ad2a8b3839468aa63170a
Santiago Hernandez <sehernandezc@sanmateo.edu.co>
2025-04-22 16:36:58 -0400

    index, slice y stride

--- listas.py
+++ listas.py
@@ -1,6 +1,6 @@
 #Lists
 
-tasks = ["task 1", "task 2", "task 3"]
+tasks = ["task 1", "task 2", "task 3", "task 4", "task 5"]
 numbers = [1, 2, 3, 4, 5]
 combined_list = [1, 2, 3, 4, 5, [6, 7, 8, 9, 10]]
 
@@ -19,5 +19,14 @@ mydictionary = {tasks[0]: "Completed",
 
 print(tasks, "\t", numbers, "\t", combined_list)
 print(mytuple, "\t", nums)
+print(nums[2])
+
+#slicing
+print(tasks[1:4])
+
+#stride
+print(tasks[0::2])
+
+#indexing dictionary elements
 print(mydictionary)
 print(mydictionary["numbers"])
